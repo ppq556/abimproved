3ef0006f5cff6bb79b96f482bbeac0e5bc0bfbd0
ppqa <malloc@linuxpl.net>
2020-08-09 11:30:25 +0200

    ab_settings.py: dodanie pliku z ustawieniami skryptu

---
+++ ab_settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# This Python file uses the following encoding: utf-8
+
+# ilość stron do pobrania z ArmyBazar (domyślnie 2 po 60 przedmiotów)
+ab_numPages = 2
+
+# tytuły/treść ogłoszeń natrętnych/powtarzalnych (regexp)
+blacklisted_leads = "^(walther GSP|PM63 RAK SEMI PM 63|.*uchwyt.*|[łl]adownica.*|kabura.*|kupi.*|latarka.*|amunicj.*|kolb.*|.*magazyne.*|.*DOWOZIMY.*|.*łuski.*|.*AMUNICJA.*|Chwyt.*|S[lł]uchawki.*|.*ulary.*|.*pocisk.*|.*Umarex.*|.*CO2.*|.*magazyn.*k.*|.*bagnet.*|.*maxim.*|.*d[pt]28.*|.*Anschutz.*|pas.*|uchyt.*|Mosin.*|.*Torb[ay].*|Konwersj.*|Przyrząd.*|Zawiesz.*|.*[lł]adowni.*|.*Pouch.*|.*Chest.*rig.*|.*szyn.*RIS.*|.*Beryl.*szyn.*|.*ok[lł]adzi.*|.*Nosid.*|Mata.*|.*Range.*bag.*|.*kabur.*|.*kurtka.*|.*etui.*|.*szelki.*|.*plecak.*|.*iglica.*|.*bezpiecznik.*|.*pazur.*|Szafa.*|.*PBS-1.*|.*PBS1.*|.*Montaż.*|Cel.*|.*sprężyn.*|.*smarow.*|.*podchwy.*|.*Norinco.*|.*[zż]elow.*|.*wynajem.*|.*Beryl.*o[zże].*|.*kompensator.*|.*Maska.*|.*Przeziernik.*|.*xgrip.*|.*wiatr[oó]wka.*|.*czyszczeni.*|.*laser.*|.*Kolce.*|.*dwójn.*)$"
+blacklisted_text = "(.*polarms.*|.*DOWOZIMY.*|.*amunicj.*|█.*)"
+
+# minimalna cena oferty do rozważenia
+minimal_price = 500
+
+# wybrane województwa
+selected_vovoidships = []
